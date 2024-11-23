1677ee76e51a99e162d18309697d4b372e6da250
luckyvishwakarmaoooooo <144231513+luckyvishwakarmaoooooo@users.noreply.github.com>
2024-11-23 14:13:23 +0000

    code configue done

---
+++ 01Javascript-Basics/02Basic.js
@@ -0,0 +1,11 @@
+let accountHolderName  = "jhon"
+var accountNumber =  121;
+const accountPassword = "123456";
+
+// prefer let in place of var keyword because issue of functional scope and block scope;
+
+accountHolderName = "sanukumari"
+accountHolderName="12345"
+let accoundState = "undefined";
+console.log("Typeof accountState : " , typeof(accoundState))
+console.table([accountHolderName , accountNumber , accountNumber , accoundState]);
